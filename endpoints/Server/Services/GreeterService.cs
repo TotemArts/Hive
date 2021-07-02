369792c44b4793c81a21c1c7d56f76977c62efeb
Zack Loveless <zack.loveless@live.com>
2021-07-02 10:24:45 -0700

    Remove changes from earlier tests.

--- endpoints/Server/Services/GreeterService.cs
+++ endpoints/Server/Services/GreeterService.cs
@@ -2,10 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Amazon.SecretsManager;
 using Grpc.Core;
-using Hive.Endpoints.Server.Extensions;
-using Hive.Endpoints.Server.Models;
 using Microsoft.Extensions.Logging;
 
 namespace Hive.Endpoints.Server
@@ -13,7 +10,6 @@ namespace Hive.Endpoints.Server
     public class GreeterService : Greeter.GreeterBase
     {
         private readonly ILogger<GreeterService> _logger;
-
         public GreeterService(ILogger<GreeterService> logger)
         {
             _logger = logger;
